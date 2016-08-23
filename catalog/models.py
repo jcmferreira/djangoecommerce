bf291d64cad3915a9cf103524f6ab5f40fe2e572
jcmferreira <julio.mferreira@gmail.com>
2016-08-23 10:02:37 -0300

    Commit do projeto, sprint atual

---
+++ catalog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    # Campos comuns
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    # Campos específicos
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['name']
+
+
+class Product(models.Model):
+    # Campos comuns
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    # Campos específicos
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+    category = models.ForeignKey('catalog.Category', verbose_name='Categoria')
+    description = models.TextField('Descrição', blank=True)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=10)
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+        ordering = ['name']
