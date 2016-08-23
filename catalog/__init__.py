91153ccf1cc5fe2d5ede55ce83fdfab6ed67a2fe
jcmferreira <julio.mferreira@gmail.com>
2016-08-23 18:45:56 -0300

    Customizando o Django Admin

---
+++ catalog/__init__.py
@@ -0,0 +1,2 @@
+# Definindo a configuração padrão da aplicação
+default_app_config = 'catalog.apps.CatalogConfig'
--- catalog/admin.py
+++ catalog/admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 from .models import Product, Category
 
 
+# Classe para gerenciar o admin da aplicação, visualmente falando
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created', 'modified']
+    search_fields = ['name', 'slug']
+    # list_filter serão os campos que estarão disponíveis para realizar um filtro na barra lateral
+    list_filter = ['created', 'modified']
+
+
+# Classe para gerenciar o admin da aplicação, visualmente falando
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'created', 'modified']
+    # Um recurso interessante do admin, na parte de campos para pesquisa, é a possibilidade de
+    # se fazer a pesquisa por um campo ForeignKey. Nesse caso, pelo nome da categoria do produto
+    # É utilizado o formato de lookup do nome da categoria no formato category__name
+    search_fields = ['name', 'slug', 'category__name']
+    # list_filter serão os campos que estarão disponíveis para realizar um filtro na barra lateral
+    list_filter = ['created', 'modified']
+
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
