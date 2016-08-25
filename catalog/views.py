124c807b66b021dfa354418cbf5add129fb1764f
jcmferreira <julio.mferreira@gmail.com>
2016-08-25 13:48:30 -0300

    Melhorando a listagem de produtos com ListView

--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Product, Category
 
 
 # Create your views here.
-def product_list(request):
+class ProductListView(generic.ListView):
+    model = Product
+    # ou
+    # queryset = Product.objects.all()
     template_name = 'catalog/product_list.html'
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, template_name, context)
+    # Aqui, a variáve de listagem do contexto é definida com context_object_name
+    # Se não for informado, será o nome da classe + _list = product_list
+    context_object_name = 'products'
 
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
+# def category(request, slug):
+#     category = Category.objects.get(slug=slug)
+#     template_name = 'catalog/category.html'
+#     context = {
+#         'current_category': category,
+#         'product_list': Product.objects.filter(category=category),
+#     }
+#     return render(request, template_name, context)
+class CategoryListView(generic.ListView):
     template_name = 'catalog/category.html'
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category),
-    }
-    return render(request, template_name, context)
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        # Esta seria a primeira forma de acessar os produtos da categoria que veio na URL
+        # category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        # return Product.objects.filter(category=category)
+        # A segunda forma seria fazer de forma direta
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
 
 
 def product(request, slug):
@@ -28,3 +46,8 @@ def product(request, slug):
         'product': product
     }
     return render(request, template_name, context)
+
+
+# Definições das views baseadas nas ClassBasedVIews
+product_list = ProductListView.as_view()
+category = CategoryListView.as_view()
--- core/views.py
+++ core/views.py
@@ -1,11 +1,43 @@
 from django.shortcuts import render
 from .forms import ContactForm
+# Views padrões do Django
+from django.views.generic import View, TemplateView
 
 
+# Opção 1
 # View principal do sistema
-def index(request):
+# def index(request):
+#    template_name = 'index.html'
+#    return render(request, template_name)
+
+
+# Opção 2
+# Aqui, a view index foi substituida por uma classe IndexView, com o método __call__
+# Isso vai fazer com que a classe possa ser "chamável" pelo Django
+# class IndexView(object):
+#     def __call__(self, request):
+#         return render(request, 'index.html')
+#
+# index é uma instancia de IndexView. Mas como a classe IndexView tem o método __call__,
+# a variável index passa a pode ser chamável
+# index = IndexView()
+
+
+# Opção 3
+# A classe acima foi reescrita usando a view-mãe do Django, a View. Agora, o método __call__ deixa de ser
+# implementado e passamos a implementar o método get
+# class IndexView(View):
+#     def get(self, request):
+#         return render(request, 'index.html')
+
+
+# Opção 4
+# Utilizando o TemplateView
+class IndexView(TemplateView):
     template_name = 'index.html'
-    return render(request, template_name)
+
+# Como a classe IndexView passou a herdar de View, a chamada precisa usar o método as_view()
+index = IndexView.as_view()
 
 
 def contact(request):
