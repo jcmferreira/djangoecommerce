cc7cb22e5e406efa510f5fb9a3d15d163186c087
jcmferreira <julio.mferreira@gmail.com>
2016-08-29 13:19:27 -0300

    Alteracao de senha com o PasswordFormView

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 # from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 from django.core.urlresolvers import reverse_lazy
 # O import login_required é usado se a view é baseada em função (def)
 # O import LoginRequiredMixin é usado se a view é baseada em classe (nosso caso)
 # from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+# Form padrão do django para fazer a alteração de senha automaticamente
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .models import User
 from .forms import UserAdminCreationForm
@@ -24,7 +26,7 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('index')
 
 
-class UpdateUserView(UpdateView):
+class UpdateUserView(LoginRequiredMixin, UpdateView):
     model = User
     template_name = 'accounts/update_user.html'
     # Definição dos campos que poderão ser atualizados
@@ -37,7 +39,21 @@ class UpdateUserView(UpdateView):
         return self.request.user
 
 
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    # Método para gerar os argumentos que serão passados para o formulário do Django
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        # Adicionando o nosso user para o formulário do Django
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
 # Definição das variáveis para as views criadas
 index = IndexView.as_view()
 register = RegisterView.as_view()
 update_user = UpdateUserView.as_view()
+update_password = UpdatePasswordView.as_view()
