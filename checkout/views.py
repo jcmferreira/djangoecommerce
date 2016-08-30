82685dac5d6f0ceecd535d02e0a9f0477de35a2c
jcmferreira <julio.mferreira@gmail.com>
2016-08-30 13:11:10 -0300

    Página do carrinho de compras, Parte II

--- checkout/views.py
+++ checkout/views.py
@@ -36,21 +36,44 @@ class CartItemView(TemplateView):
 
     template_name = 'checkout/cart.html'
 
+    def get_formset(self, clear=False):
+        CartItemFormSet = modelformset_factory(CartItem, fields=['quantity'], can_delete=True, extra=0)
+        session_key = self.request.session.session_key
+
+        # Aqui, é validada a sessão do usuário. Se sim, é pq ele já fez algo e o carrinho dele será buscado
+        # Se não existir, será criado um CartItem.objects.none(), que nada mais é que uma queryset vazia
+        if session_key:
+            if clear:
+                formset = CartItemFormSet(queryset=CartItem.objects.filter(cart_key=session_key))
+            else:
+                formset = CartItemFormSet(
+                    queryset=CartItem.objects.filter(cart_key=session_key),
+                    data=self.request.POST or None
+                )
+        else:
+            formset = CartItemFormSet(queryset=CartItem.objects.none())
+        return formset
+
     # Esse método é sobrescrito sempre que se deseja modificar o contexto do template, adicionando ou modificando os
     # valores que serão disponibilizados no template durante o render da view
     def get_context_data(self, **kwargs):
         context = super(CartItemView, self).get_context_data(**kwargs)
-        CartItemFormSet = modelformset_factory(CartItem, fields=['quantity'], can_delete=True, extra=0)
-
-        # Aqui, é validada a sessão do usuário. Se sim, é pq ele já fez algo e o carrinho dele será buscado
-        # Se não existir, será criado um CartItem.objects.none(), que nada mais é que uma queryset vazia
-        session_key = self.request.session.session_key
-        if session_key:
-            context['formset'] = CartItemFormSet(queryset=CartItem.objects.filter(cart_key=session_key))
-        else:
-            context['formset'] = CartItemFormSet(queryset=CartItem.objects.none())
-
+        context['formset'] = self.get_formset()
         return context
+
+    # Método para realizar o post do carrinho
+    # Métodos disponíveis: get, post, put, delete. Todos de acordo com o method definido no formulário HTML
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        # Aqui, é realizado o padrão do método post, recuperando o contexto e renderizando o response com os kwargs
+        context = self.get_context_data(**kwargs)
+        # Se o formulário estiver válido, ele deverá ser salvo e o parâmetro clear irá como True
+        if formset.is_valid():
+            formset.save()
+            messages.success(request, 'Carrinho atualizado com sucesso!')
+            context['formset'] = self.get_formset(clear=True)
+        # Aqui, sempre deve ser retornado o render_to_response. Ela usa o conteúdo de template_name e usa o context
+        return self.render_to_response(context)
 
 # Definindo a variável da view para uso nas URLs
 create_cartitem = CreateCartItemView.as_view()
